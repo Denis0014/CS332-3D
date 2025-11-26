55ca774d09024b312572efb69f2a59fea7c3a055
Denis <109104246+Denis0014@users.noreply.github.com>
2025-11-26 16:09:42 +0300

    Add files via upload

---
+++ affine.py
@@ -0,0 +1,369 @@
+import tkinter, tkinter.messagebox
+import numpy as np
+from tkinter import simpledialog
+from geometry import BaseCanvas, Edge, ITransformable, Point, PointType, Polygon, PolygonType
+from typing import Any, Callable, Iterator, Sequence
+
+FOV = 120  # Field of view in degrees
+
+class TkinterCanvas(BaseCanvas):
+    def __init__(self, width: int, height: int) -> None:
+        super().__init__(width, height)
+        self.tk_canvas = tkinter.Canvas(width=width, height=height)
+        self.tk_canvas.pack()
+        root = self.tk_canvas.winfo_toplevel()
+        self.create_menubar(root)
+        self.last_action: dict[str, Any] = {}
+
+    def create_menubar(self, root: tkinter.Tk | tkinter.Toplevel) -> None:
+        menubar = tkinter.Menu(root)
+        file_menu = tkinter.Menu(menubar, tearoff=0)
+        file_menu.add_command(label="Exit", command=root.quit)
+        menubar.add_cascade(label="File", menu=file_menu)
+        transformations_menu = tkinter.Menu(menubar, tearoff=1)
+        transformations_menu.add_command(label="Scale", command=self.apply_scale)
+        transformations_menu.add_command(label="Translate", command=self.apply_translation)
+        transformations_menu.add_command(label="Rotate Around Point", command=self.apply_rotation_around_point)
+        transformations_menu.add_command(label="Rotate Around Center", command=self.apply_rotation_around_center)
+        transformations_menu.add_command(label="Reflect", command=self.apply_reflection)
+        transformations_menu.add_separator()
+        transformations_menu.add_command(label="Repeat Last Action", command=self.repeat_last_action)
+        menubar.add_cascade(label="Transformations", menu=transformations_menu)
+        root.config(menu=menubar)
+    
+    @staticmethod
+    def projection_onto_2d(points: Sequence[PointType], width: int, height: int) -> Iterator[Point]:
+        aspect = width / height
+        fov = np.radians(FOV)
+
+        matrix = np.array([
+            [1 / (aspect * np.tan(fov / 2)), 0, 0, 0],
+            [0, 1 / np.tan(fov / 2), 0, 0],
+            [0, 0, 1, -1],
+            [0, 0, 1, 0]
+        ], dtype=float)
+
+        for point in points:
+            vec = np.array([point.x, point.y, point.z, 1])
+            transformed_vec = vec @ matrix
+            if transformed_vec[3] != 0:
+                x_ndc = transformed_vec[0] / transformed_vec[3]
+                y_ndc = transformed_vec[1] / transformed_vec[3]
+            else:
+                x_ndc = transformed_vec[0]
+                y_ndc = transformed_vec[1]
+
+            x_screen = (1 - x_ndc) * 0.5 * width
+            y_screen = (y_ndc + 1) * 0.5 * height
+
+            print(f"3D Point ({point.x}, {point.y}, {point.z}) -> 2D Point ({x_screen}, {y_screen})")
+
+            yield Point(x_screen, y_screen, 0)
+    
+    def draw_shapes(self) -> None:
+        self.tk_canvas.delete("all")
+        print("--- Projection Debug ---")
+
+        for point in self.projection_onto_2d(self.points, self.width, self.height):
+            self.tk_canvas.create_oval(
+                point.x - 2, point.y - 2,
+                point.x + 2, point.y + 2,
+                fill='black'
+            )
+
+        for edge in self.edges:
+            for p1, p2 in zip(
+                self.projection_onto_2d(edge.points, self.width, self.height),
+                self.projection_onto_2d(edge.points[1:], self.width, self.height)
+            ):
+                self.tk_canvas.create_line(p1.x, p1.y, p2.x, p2.y, fill='black')
+
+        # for polygon in self.polygons:
+        #     points = [(point.x, point.y) for edge in polygon for point in self.projection_onto_2d(edge.points)]
+        #     self.tk_canvas.create_polygon(points, outline='black', fill='', width=1)
+
+    def clear(self) -> None:
+        super().clear()
+        self.tk_canvas.delete("all")
+
+    def get_polygon(self) -> PolygonType | None:
+        num = simpledialog.askinteger(
+            "Input for polygon", "Enter number of polygon"
+        )
+
+        if num is None or not self.polygons or len(self.polygons) < num or num < 1:
+            tkinter.messagebox.showerror(
+                "Invalid polygon",
+                "Invalid polygon number."
+            )
+            return None
+        
+        return self.polygons[num - 1]
+    
+    def highlight_polygon(self, polygon: PolygonType) -> None:
+        for edge in polygon:
+            for p1, p2 in zip(
+                self.projection_onto_2d(edge.points, self.width, self.height),
+                self.projection_onto_2d(edge.points[1:], self.width, self.height)
+            ):
+                self.tk_canvas.create_line(p1.x, p1.y, p2.x, p2.y, fill='red', width=1, dash=(4, 2))
+
+    @staticmethod
+    def with_point_selection(func: Callable[..., Any]) -> Callable[..., Any]:
+        def ask() -> Iterator[float | None]:
+            for axis in ['x', 'y', 'z']:
+                yield simpledialog.askfloat(
+                    f"Input for {axis}-coordinate", 
+                    f"Enter {axis}-coordinate of the point:"
+                )
+                
+        def wrapper(obj: ITransformable, *args: Any, **kwargs: Any) -> Any:
+            x, y, z = ask()
+            if x is None or y is None or z is None:
+                return
+            
+            point = Point(x, y, z)
+            return obj.point_selected(func, point, *[obj, *args], **kwargs)
+        
+        return wrapper
+    
+    def apply_scale(self, **kwargs: Any) -> None:
+        def ask() -> Iterator[float | None]:
+            for axis in ['sx', 'sy', 'sz']:
+                yield simpledialog.askfloat(
+                    f"Input for {axis}", 
+                    f"Enter scaling factor for {axis}-axis:"
+                )
+
+        try:
+            obj = kwargs.get("object") or self.get_polygon()
+            if obj is None or not isinstance(obj, Polygon):
+                tkinter.messagebox.showerror(
+                    "Invalid polygon",
+                    "Selected polygon is not transformable."
+                )
+                return
+            self.highlight_polygon(obj)
+            
+            sx, sy, sz = kwargs.get("factors") or ask()
+            if sx is None or sy is None or sz is None:
+                tkinter.messagebox.showerror(
+                    "Invalid scaling factors",
+                    "Invalid scaling factors. Please enter valid numbers for all axes."
+                )
+                return
+
+            obj.scale_from_center(sx, sy, sz)
+            self.last_action = {
+                "action": self.scale_from_center,
+                "kwargs": {"object": obj, "factors": (sx, sy, sz)}
+            }
+        finally:
+            self.draw_shapes()
+
+    def apply_translation(self, **kwargs: Any) -> None:
+        def ask() -> Iterator[float | None]:
+            for axis in ['tx', 'ty', 'tz']:
+                yield simpledialog.askfloat(
+                    f"Input for {axis}", 
+                    f"Enter translation distance for {axis}-axis:"
+                )
+        
+        try:
+            obj = kwargs.get("object") or self.get_polygon()
+            if obj is None or not isinstance(obj, Polygon):
+                tkinter.messagebox.showerror(
+                    "Invalid polygon",
+                    "Selected polygon is not transformable."
+                )
+                return
+            self.highlight_polygon(obj)
+            
+            tx, ty, tz = ask()
+            if tx is None or ty is None or tz is None:
+                tkinter.messagebox.showerror(
+                    "Invalid translation",
+                    "Invalid translation distances. Please enter valid numbers for all axes."
+                )
+                return
+            
+            obj.translate(tx, ty, tz)
+            self.last_action = {
+                "action": self.apply_translation,
+                "kwargs": {"object": obj, "distances": (tx, ty, tz)}
+            }
+        finally:
+            self.draw_shapes()
+
+    def apply_rotation_around_center(self, **kwargs: Any) -> None:
+        def ask() -> Iterator[float | None]:
+            for axis in ['angle_x', 'angle_y', 'angle_z']:
+                yield simpledialog.askfloat(
+                    f"Input for {axis}", 
+                    f"Enter rotation angle (in degrees) around {axis}-axis:"
+                )
+
+        try:
+            obj = kwargs.get("object") or self.get_polygon()
+            if obj is None or not isinstance(obj, Polygon):
+                tkinter.messagebox.showerror(
+                    "Invalid polygon",
+                    "Selected polygon is not transformable."
+                )
+                return
+            self.highlight_polygon(obj)
+                    
+            angle_x, angle_y, angle_z = kwargs.get("angles") or ask()
+            if angle_x is None or angle_y is None or angle_z is None:
+                tkinter.messagebox.showerror(
+                    "Invalid angles",
+                    "Invalid rotation angles. Please enter valid numbers for all axes."
+                )
+                return
+            
+            obj.rotate(angle_x, angle_y, angle_z)
+            self.last_action = {
+                "action": self.apply_rotation_around_center,
+                "kwargs": {"object": obj, "angles": (angle_x, angle_y, angle_z)}
+            }
+        finally:
+            self.draw_shapes()
+
+    @with_point_selection
+    def apply_rotation_around_point(self, **kwargs: Any) -> None:
+        def ask() -> Iterator[float | None]:
+            for axis in ['angle_x', 'angle_y', 'angle_z']:
+                yield simpledialog.askfloat(
+                    f"Input for {axis}", 
+                    f"Enter rotation angle (in degrees) around {axis}-axis:"
+                )
+
+        try:
+            obj = kwargs.get("object") or self.get_polygon()
+            if obj is None or not isinstance(obj, Polygon):
+                tkinter.messagebox.showerror(
+                    "Invalid polygon",
+                    "Selected polygon is not transformable."
+                )
+                return
+            self.highlight_polygon(obj)
+                    
+            angle_x, angle_y, angle_z = kwargs.get("angles") or ask()
+            if angle_x is None or angle_y is None or angle_z is None:
+                tkinter.messagebox.showerror(
+                    "Invalid angles",
+                    "Invalid rotation angles. Please enter valid numbers for all axes."
+                )
+                return
+            
+            obj.rotate(angle_x, angle_y, angle_z)
+            self.last_action = {
+                "action": self.apply_rotation_around_point,
+                "kwargs": {"object": obj, "angles": (angle_x, angle_y, angle_z)}
+            }
+        finally:
+            self.draw_shapes()
+
+    def apply_reflection(self, **kwargs: Any) -> None:
+        try:
+            obj = kwargs.get("object") or self.get_polygon()
+            if obj is None or not isinstance(obj, Polygon):
+                tkinter.messagebox.showerror(
+                    "Invalid polygon",
+                    "Selected polygon is not transformable."
+                )
+                return
+            self.highlight_polygon(obj)
+
+            axis = kwargs.get("axis") or simpledialog.askstring(
+                "Input for reflection", 
+                "Enter axis of reflection (x, y, or z):"
+            )
+            if axis not in ('x', 'y', 'z'):
+                tkinter.messagebox.showerror(
+                    "Invalid axis",
+                    "Invalid axis of reflection. Please enter 'x', 'y' or 'z'."
+                )
+                return
+            
+            obj.reflect(axis)
+            self.last_action = {
+                "action": self.apply_reflection,
+                "kwargs": {"object": obj, "axis": axis}
+            }
+        finally:
+            self.draw_shapes()
+
+    def repeat_last_action(self) -> None:
+        if not self.last_action:
+            tkinter.messagebox.showinfo(
+                "No action to repeat",
+                "There is no last action to repeat."
+            )
+            return
+        
+        action = self.last_action.get("action")
+        kwargs = self.last_action.get("kwargs", {})
+        if action:
+            action(**kwargs)
+
+if __name__ == "__main__":
+    canvas = TkinterCanvas(400, 400)
+
+    canvas += Polygon([
+        Edge([
+            Point(-30, -30, 50),
+            Point(30, -30, 50),
+            Point(30, 30, 50),
+            Point(-30, 30, 50),
+            Point(-30, -30, 50)
+        ]),
+        Edge([
+            Point(-30, -30, 250),
+            Point(30, -30, 250),
+            Point(30, 30, 250),
+            Point(-30, 30, 250),
+            Point(-30, -30, 250)
+        ]),
+        Edge([
+            Point(-30, -30, 50),
+            Point(-30, -30, 250)
+        ]),
+        Edge([
+            Point(30, -30, 50),
+            Point(30, -30, 250)
+        ]),
+        Edge([
+            Point(30, 30, 50),
+            Point(30, 30, 250)
+        ]),
+        Edge([
+            Point(-30, 30, 50),
+            Point(-30, 30, 250)
+        ])
+    ])
+
+    # canvas += Polygon([
+    # Edge([
+    #     Point(1, 0, 1),
+    #     Point(2, 40, 50),
+    #     Point(50, 50, 50),
+    #     Point(40, 40, 50)
+    # ]),
+    # Edge([
+    #     Point(40, 40, 50),
+    #     Point(42, 40, 50),
+    # ]),
+    # Edge([
+    #     Point(50, 40, 50),
+    #     Point(42, 40, 50),
+    # ]),
+    # Edge([
+    #     Point(50, 50, 50),
+    #     Point(42, 40, 50),
+    # ]),
+    # ])
+    # print(canvas)
+
+    canvas.draw_shapes()
+    tkinter.mainloop()
---
+++ geometry.py
@@ -0,0 +1,402 @@
+from __future__ import annotations
+import numpy as np
+from typing import Callable, Dict, Iterator, List, Literal, Union, Any, Sequence, Self, override
+
+Number = Union[int, float]
+PointType = Union["BasePoint", "Point"]
+EdgeType = Union["BaseEdge", "Edge"]
+PolygonType = Union["BasePolygon", "Polygon"]
+Shape = Union[PointType, EdgeType, PolygonType]
+
+
+class ITransformable:
+    def __init__(self) -> None:
+        self.points: List[PointType] = self.get_shape_points()
+
+    def get_shape_points(self) -> List[PointType]:
+        raise NotImplementedError("This method should be implemented in subclasses.")
+    
+    def centered(self, func: Callable[..., Self], *args: Any, **kwargs: Any) -> Self:
+        center_x = sum(point.x for point in self.points) / len(self.points)
+        center_y = sum(point.y for point in self.points) / len(self.points)
+        center_z = sum(point.z for point in self.points) / len(self.points)
+
+        self.translate(-center_x, -center_y, -center_z)
+        result = func(*args, **kwargs)
+        self.translate(center_x, center_y, center_z)
+        return result
+
+    def point_selected(self, func: Callable[..., Self], point: PointType, *args: Any, **kwargs: Any) -> Self:
+        self.translate(-point.x, -point.y, -point.z) if point else None
+        result = func(*args, **kwargs)
+        self.translate(point.x, point.y, point.z) if point else None
+        return result
+
+    def transform(self, matrix: np.ndarray) -> Self:
+        for point in self.points:
+            vec = np.array([point.x, point.y, point.z, 1])
+            transformed_vec = vec @ matrix
+            point.x = transformed_vec[0] / transformed_vec[3]
+            point.y = transformed_vec[1] / transformed_vec[3]
+            point.z = transformed_vec[2] / transformed_vec[3]
+        return self
+    
+    def projection(self, matrix: np.ndarray) -> Iterator[BasePoint]:
+        for point in self.points:
+            vec = np.array([point.x, point.y, point.z, 1])
+            transformed_vec = matrix @ vec
+            yield BasePoint(
+                transformed_vec[0] / transformed_vec[3],
+                transformed_vec[1] / transformed_vec[3],
+                transformed_vec[2] / transformed_vec[3],
+            )
+
+    def scale(self, sx: Number, sy: Number, sz: Number) -> Self:
+        scale_matrix = np.array([
+            [sx, 0, 0, 0],
+            [0, sy, 0, 0],
+            [0, 0, sz, 0],
+            [0, 0, 0, 1]
+        ])
+        return self.transform(scale_matrix)
+    
+    def scale_from_center(self, sx: Number, sy: Number, sz: Number) -> Self:
+        return self.centered(self.scale, sx, sy, sz)
+    
+    def scale_from_point(self, point: PointType, sx: Number, sy: Number, sz: Number) -> Self:
+        return self.point_selected(self.scale, point, sx, sy, sz)
+    
+    def translate(self, tx: Number, ty: Number, tz: Number) -> Self:
+        translation_matrix = np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+            [0, 0, 1, 0],
+            [tx, ty, tz, 1]
+        ])
+        return self.transform(translation_matrix)
+    
+    def rotate(self, angle_x: Number, angle_y: Number, angle_z: Number) -> Self:
+        rx = np.radians(angle_x)
+        ry = np.radians(angle_y)
+        rz = np.radians(angle_z)
+
+        rotation_x = np.array([
+            [1, 0, 0, 0],
+            [0, np.cos(rx), np.sin(rx), 0],
+            [0, -np.sin(rx), np.cos(rx), 0],
+            [0, 0, 0, 1]
+        ])
+
+        rotation_y = np.array([
+            [np.cos(ry), 0, -np.sin(ry), 0],
+            [0, 1, 0, 0],
+            [np.sin(ry), 0, np.cos(ry), 0],
+            [0, 0, 0, 1]
+        ])
+
+        rotation_z = np.array([
+            [np.cos(rz), np.sin(rz), 0, 0],
+            [-np.sin(rz), np.cos(rz), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
+
+        print(rotation_x)
+        print(rotation_y)
+        print(rotation_z)
+
+        rotation_matrix = rotation_z @ rotation_y @ rotation_x
+        return self.transform(rotation_matrix)
+    
+    def reflect(self, axis: Literal['x', 'y', 'z']) -> Self:
+        if axis == 'x':
+            reflect_matrix = np.array([
+                [-1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]
+            ])
+        elif axis == 'y':
+            reflect_matrix = np.array([
+                [1, 0, 0, 0],
+                [0, -1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]
+            ])
+        elif axis == 'z':
+            reflect_matrix = np.array([
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, -1, 0],
+                [0, 0, 0, 1]
+            ])
+        else:
+            raise ValueError("Axis must be 'x', 'y', or 'z'.")
+
+        return self.transform(reflect_matrix)
+
+
+class BaseShape(type):
+    pass
+
+
+class BasePoint(metaclass=BaseShape):
+    def __init__(self, x: Number, y: Number, z: Number) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z}, id={hex(id(self))})"
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, BasePoint):
+            return False
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    def __ne__(self, value: object) -> bool:
+        return not self.__eq__(value)
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y, self.z))
+    
+    def __iter__(self):
+        yield self.x
+        yield self.y
+        yield self.z
+
+
+class Point(BasePoint, ITransformable):
+    def __init__(self, x: Number, y: Number, z: Number) -> None:
+        BasePoint.__init__(self, x, y, z)
+        ITransformable.__init__(self)
+        self.__features: Dict[str, Any] = dict()
+
+    @override
+    def get_shape_points(self) -> List[PointType]:
+        return [self]
+
+    def __getitem__(self, item: str) -> Any:
+        return self.__features.get(item, None)
+    
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.__features[key] = value
+
+    def __repr__(self) -> str:
+        features = ", ".join(f"{k}={repr(v)}" for k, v in self.__features.items())
+        if features:
+            return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z}, {features}, id={hex(id(self))})"
+        
+        else:
+            return f"{self.__class__.__name__}({self.x}, {self.y}, {self.z}, id={hex(id(self))})"
+
+
+class BaseEdge(metaclass=BaseShape):
+    def __init__(self, points: Sequence[PointType]) -> None:
+        self.points: List[PointType] = list(points)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.points}, id={hex(id(self))})"
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, BaseEdge):
+            return False
+        return self.points == other.points
+    
+    def __ne__(self, value: object) -> bool:
+        return not self.__eq__(value)
+    
+    def __add__(self, other: Union[PointType, EdgeType]) -> Self:
+        if isinstance(other, BaseEdge):
+            return self.__class__(**self.__dict__, points=self.points + other.points)
+        
+        else:
+            return self.__class__(**self.__dict__, points=self.points + [other])
+        
+    def __iadd__(self, other: Union[PointType, EdgeType]) -> Self:
+        if isinstance(other, BaseEdge):
+            self.points.extend(other.points)
+
+        else:
+            self.points.append(other)
+
+        return self
+    
+    def __hash__(self) -> int:
+        return hash(tuple(self.points))
+    
+    def __len__(self) -> int:
+        return len(self.points)
+    
+    def __iter__(self):
+        for point in self.points:
+            yield point
+
+
+class Edge(BaseEdge, ITransformable):
+    def __init__(self, points: Sequence[PointType]) -> None:
+        BaseEdge.__init__(self, points)
+        ITransformable.__init__(self)
+        self.__features: Dict[str, Any] = dict()
+
+    @override
+    def get_shape_points(self) -> List[PointType]:
+        return self.points
+    
+    def __getitem__(self, item: str) -> Any:
+        return self.__features.get(item, None)
+    
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.__features[key] = value
+
+    def __repr__(self) -> str:
+        features = ", ".join(f"{k}={repr(v)}" for k, v in self.__features.items())
+        if features:
+            return f"{self.__class__.__name__}({self.points}, {features}, id={hex(id(self))})"
+        
+        else:
+            return f"{self.__class__.__name__}({self.points})"
+
+
+class BasePolygon(metaclass=BaseShape):
+    def __init__(self, edges: Sequence[EdgeType]) -> None:
+        self.edges: List[EdgeType] = list(edges)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.edges}, id={hex(id(self))})"
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, BasePolygon):
+            return False
+        return self.edges == other.edges
+    
+    def __ne__(self, value: object) -> bool:
+        return not self.__eq__(value)
+    
+    def __len__(self) -> int:
+        return len(self.edges)
+    
+    def __add__(self, other: EdgeType) -> Self:
+        return self.__class__(**self.__dict__, edges=self.edges + [other])
+    
+    def __iadd__(self, other: EdgeType) -> Self:
+        self.edges.append(other)
+        return self
+    
+    def __hash__(self) -> int:
+        return hash(tuple(self.edges))
+    
+    def __iter__(self):
+        for edge in self.edges:
+            yield edge
+
+
+class Polygon(BasePolygon, ITransformable):
+    def __init__(self, edges: Sequence[EdgeType]) -> None:
+        BasePolygon.__init__(self, edges)
+        ITransformable.__init__(self)
+        self.__features: Dict[str, Any] = dict()
+
+    @override
+    def get_shape_points(self) -> List[PointType]:
+        points: List[PointType] = []
+        for edge in self.edges:
+            points.extend(edge.points)
+        return points
+    
+    def __getitem__(self, item: str) -> Any:
+        return self.__features.get(item, None)
+    
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.__features[key] = value
+
+    def __repr__(self) -> str:
+        features = ", ".join(f"{k}={repr(v)}" for k, v in self.__features.items())
+        if features:
+            return f"{self.__class__.__name__}({self.edges}, {features}, id={hex(id(self))})"
+        
+        else:
+            return f"{self.__class__.__name__}({self.edges}, id={hex(id(self))})"
+
+
+class BaseCanvas(ITransformable):
+    def __init__(
+            self,
+            width: int,
+            height: int,
+            points: Sequence[PointType] = (),
+            edges: Sequence[EdgeType] = (),
+            polygons: Sequence[PolygonType] = (),
+        ) -> None:
+        self.width = width
+        self.height = height
+        self.points: List[PointType] = list(points)
+        self.edges: List[EdgeType] = list(edges)
+        self.polygons: List[PolygonType] = list(polygons)
+
+    @override
+    def get_shape_points(self) -> List[PointType]:
+        return self.points
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(width={self.width}, height={self.height}, "
+            f"polygons={self.polygons}, id={hex(id(self))})"
+        )
+    
+    def __add__(self, shape: Shape) -> Self:
+        new = self.__class__(**self.__dict__)
+        if (isinstance(point := shape, BasePoint)):
+            if point not in new.points:
+                new.points.append(point)
+        elif isinstance(edge := shape, BaseEdge):
+            for point in edge:
+                new = new.__add__(point)
+            if edge not in new.edges:
+                new.edges.append(edge)
+        elif isinstance(polygon := shape, BasePolygon):
+            for edge in polygon:
+                new = new.__add__(edge)
+            if polygon not in new.polygons:
+                new.polygons.append(polygon)
+        return new
+    
+    def __iadd__(self, shape: Shape) -> Self:
+        if (isinstance(point := shape, BasePoint)):
+            if point not in self.points:
+                self.points.append(point)
+        elif isinstance(edge := shape, BaseEdge):
+            for point in edge:
+                self.__iadd__(point)
+            if edge not in self.edges:
+                self.edges.append(edge)
+        elif isinstance(polygon := shape, BasePolygon):
+            for edge in polygon:
+                self.__iadd__(edge)
+            if polygon not in self.polygons:
+                self.polygons.append(polygon)
+        return self
+    
+    def __sub__(self, shape: Shape) -> Self:
+        new = self.__class__(**self.__dict__)
+        if isinstance(shape, BasePoint):
+            new.points.remove(shape)
+        elif isinstance(shape, BaseEdge):
+            new.edges.remove(shape)
+        else:
+            new.polygons.remove(shape)
+        return new
+    
+    def __isub__(self, shape: Shape) -> Self:
+        if isinstance(shape, BasePoint):
+            self.points.remove(shape)
+        elif isinstance(shape, BaseEdge):
+            self.edges.remove(shape)
+        else:
+            self.polygons.remove(shape)
+        return self
+    
+    def clear(self) -> None:
+        self.points.clear()
+        self.edges.clear()
+        self.polygons.clear()
